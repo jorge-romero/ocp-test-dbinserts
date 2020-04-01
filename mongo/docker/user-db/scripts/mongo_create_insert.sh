5e8e34930ef400b32ade8d539e593352515f6d45
Jorge Romero <jorge.romeropalma@gmail.com>
2020-04-01 17:41:26 +0200

    fixes

--- mongo/docker/user-db/scripts/mongo_create_insert.sh
+++ mongo/docker/user-db/scripts/mongo_create_insert.sh
@@ -6,8 +6,8 @@ MONGO_SERVICE=$1
 #mongod --fork --logpath /var/log/mongodb.log --dbpath /data/db/
 
 FILES=$SCRIPT_DIR/*-create.js
-for f in $FILES; do mongo $MONGO_SERVICE -u $MONGODB_USER -p $MONGODB_PASSWORD users $f; done
+for f in $FILES; do mongo $MONGO_SERVICE/users -u $MONGODB_USER -p $MONGODB_PASSWORD users $f; done
 
 FILES=$SCRIPT_DIR/*-insert.js
-for f in $FILES; do mongo $MONGO_SERVICE -u $MONGODB_USER -p $MONGODB_PASSWORD users $f; done
+for f in $FILES; do mongo $MONGO_SERVICEi/users -u $MONGODB_USER -p $MONGODB_PASSWORD users $f; done
 

257ae4d842cc4c367fa48ce1dc687e9e6c5cf25c
Jorge Romero <jorge.romeropalma@gmail.com>
2020-04-01 17:06:53 +0200

    fixes centos

--- mongo/docker/user-db/scripts/mongo_create_insert.sh
+++ mongo/docker/user-db/scripts/mongo_create_insert.sh
@@ -2,14 +2,12 @@
 
 SCRIPT_DIR=$(dirname "$0")
 MONGO_SERVICE=$1
-USER=$2
-PWD=$3
 
 #mongod --fork --logpath /var/log/mongodb.log --dbpath /data/db/
 
 FILES=$SCRIPT_DIR/*-create.js
-for f in $FILES; do mongo $MONGO_SERVICE -u $USER -p $PWD users $f; done
+for f in $FILES; do mongo $MONGO_SERVICE -u $MONGODB_USER -p $MONGODB_PASSWORD users $f; done
 
 FILES=$SCRIPT_DIR/*-insert.js
-for f in $FILES; do mongo $MONGO_SERVICE -u $USER -p $PWD users $f; done
+for f in $FILES; do mongo $MONGO_SERVICE -u $MONGODB_USER -p $MONGODB_PASSWORD users $f; done
 
